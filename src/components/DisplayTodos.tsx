c0d50452038cf47a1f5d9561a799aefcc832c487
Narathip <narathip2002@outlook.co.th>
2024-07-31 23:34:14 +0700

    if !tasks show some text to tell no task here

--- src/components/DisplayTodos.tsx
+++ src/components/DisplayTodos.tsx
@@ -80,71 +80,82 @@ const DisplayTodos: React.FC<DisplayTodosProps> = ({ tasks, setTasks }) => {
         filteredTasks={filteredTasks}
       />
 
-      {filteredTasks.map((task) => (
-        <div
-          key={task.id}
-          className="p-4 flex flex-col bg-white dark:bg-base-100 rounded-lg"
-        >
-          <div className="flex justify-between items-center">
-            <div className="flex items-center gap-4">
-              <input
-                type="checkbox"
-                className="checkbox"
-                checked={task.isComplete}
-                onChange={() => handleTaskCheckboxChange(task.id)}
-              />
-              <div>
-                <p className="text-xl font-medium">{task.title}</p>
-                <p>{task.tag}</p>
+      {tasks.length === 0 ? (
+        <div className="py-32 flex flex-col gap-2 justify-center items-center">
+          <h1 className="text-3xl font-semibold">
+            {" "}
+            There's nothing to do yet,{" "}
+          </h1>
+          <h1 className="text-4xl font-bold">so add something to do now.</h1>
+        </div>
+      ) : (
+        filteredTasks.map((task) => (
+          <div
+            key={task.id}
+            className="p-4 flex flex-col bg-white dark:bg-base-100 rounded-lg"
+          >
+            <div className="flex justify-between items-center">
+              <div className="flex items-center gap-4">
+                <input
+                  type="checkbox"
+                  className="checkbox"
+                  checked={task.isComplete}
+                  onChange={() => handleTaskCheckboxChange(task.id)}
+                />
+                <div>
+                  <p className="text-xl font-medium">{task.title}</p>
+                  <p>{task.tag}</p>
+                </div>
+              </div>
+              <div className="flex items-center gap-4">
+                {task.subTask && task.subTask.length > 0 && (
+                  <button
+                    className="btn btn-square"
+                    onClick={() => toggleExpandSubTasks(task.id)}
+                  >
+                    {expandSubTasks.includes(task.id) ? (
+                      <ChevronUp />
+                    ) : (
+                      <ChevronDown />
+                    )}
+                  </button>
+                )}
+                <button
+                  className="btn btn-square hover:bg-orange-500"
+                  onClick={() => handleEdit(task)}
+                >
+                  <SquarePen className="hover:text-white" />
+                </button>
+                <button
+                  className="btn btn-square hover:bg-red-500"
+                  onClick={() => handleDelete(task.id)}
+                >
+                  <Trash className="hover:text-white" />
+                </button>
               </div>
             </div>
-            <div className="flex items-center gap-4">
-              {task.subTask && task.subTask.length > 0 && (
-                <button
-                  className="btn btn-square"
-                  onClick={() => toggleExpandSubTasks(task.id)}
-                >
-                  {expandSubTasks.includes(task.id) ? (
-                    <ChevronUp />
-                  ) : (
-                    <ChevronDown />
-                  )}
-                </button>
-              )}
-              <button
-                className="btn btn-square hover:bg-orange-500"
-                onClick={() => handleEdit(task)}
-              >
-                <SquarePen className="hover:text-white" />
-              </button>
-              <button
-                className="btn btn-square hover:bg-red-500"
-                onClick={() => handleDelete(task.id)}
-              >
-                <Trash className="hover:text-white" />
-              </button>
-            </div>
-          </div>
-          {expandSubTasks.includes(task.id) &&
-            task.subTask &&
-            task.subTask.length > 0 && (
-              <div className="mt-4 ml-8">
-                {task.subTask.map((subTask) => (
-                  <div
-                    key={subTask.id}
-                    className="flex justify-between items-center mb-2 p-2 bg-gray-100 dark:bg-base-200 rounded-lg"
-                  >
-                    <div className="flex items-center gap-4">
-                      <div>
-                        <p className="text-lg">{subTask.title}</p>
+            {expandSubTasks.includes(task.id) &&
+              task.subTask &&
+              task.subTask.length > 0 && (
+                <div className="mt-4 ml-8">
+                  {task.subTask.map((subTask) => (
+                    <div
+                      key={subTask.id}
+                      className="flex justify-between items-center mb-2 p-2 bg-gray-100 dark:bg-base-200 rounded-lg"
+                    >
+                      <div className="flex items-center gap-4">
+                        <div>
+                          <p className="text-lg">{subTask.title}</p>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                ))}
-              </div>
-            )}
-        </div>
-      ))}
+                  ))}
+                </div>
+              )}
+          </div>
+        ))
+      )}
+
       {editTask && (
         <ModalTodo
           isOpen={isModalOpen}
